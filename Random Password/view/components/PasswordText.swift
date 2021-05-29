b0a60867ac24185ade45d08b14a74a3640df0a34
Pedro Lobato <pedro.lobato@meta.com.br>
2021-05-29 18:33:08 -0300

    Create folder component

---
+++ Random Password/view/components/PasswordText.swift
@@ -0,0 +1,20 @@
+//
+//  PasswordText.swift
+//  Random Password
+//
+//  Created by Pedro Lobato on 29/05/21.
+//
+
+import SwiftUI
+
+struct PasswordText: View {
+    var body: some View {
+        Text("Generated Password")
+    }
+}
+
+struct PasswordText_Previews: PreviewProvider {
+    static var previews: some View {
+        PasswordText()
+    }
+}
