74e00b2ccaa87d8d78e8eb6aef0a8dc61b15cc21
KassioVieira <kassio.vieira7@gmail.com>
2021-05-29 17:49:34 -0300

    structure mvc

---
+++ Random Password/model/Password.swift
@@ -0,0 +1,15 @@
+//
+//  Password.swift
+//  Random Password
+//
+//  Created by Kássio Vieira da Luz on 29/05/21.
+//
+
+import Foundation
+
+struct Password {
+    let text: String;
+    let hasNumber: Bool;
+    let hasLetter: Bool;
+    let hasSymbols: Bool;
+}
